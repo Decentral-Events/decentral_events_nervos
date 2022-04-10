6b4265b259049b577f6d92aa186271d5aa1a4e26
Yash Goyal <yashgo0018@gmail.com>
2022-04-10 20:34:08 +0530

    fixed a bug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -50,11 +50,11 @@ export default class App extends Component {
     }
     const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
-    await provider.send("eth_requestAccounts", []);
-    const signer = provider.getSigner();
-    const address = await signer.getAddress();
     try {
       if (localStorage.getItem("auth")) {
+        await provider.send("eth_requestAccounts", []);
+        const signer = provider.getSigner();
+        const address = await signer.getAddress();
         const {
           user,
           token,
